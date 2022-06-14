6220579a4de8958e183e89c897f607e080cba374
Felipe <39858416+felpsgk@users.noreply.github.com>
2022-06-14 15:55:19 -0300

    Update conexao.php

--- controller/conexao.php
+++ controller/conexao.php
@@ -1,27 +1,22 @@
-<?php
-
 //session_start();
 
-    $servername = "localhost:3306"; /* pode deixar localhost */
+    $servername = "localhost"; /* pode deixar localhost */
 
-    $username = "root"; /* nome do usuario do banco de dados */ 
+    $username = "hnrtco66_felpsgk"; /* nome do usuario do banco de dados */ 
 
-    $password = "root"; /* senha do banco de dados caso exista senao deixa $password = "" */
+    $password = "rootdoadmin"; /* senha do banco de dados caso exista senao deixa $password = "" */
 
-    $dbname = "hnrtco66_fvgweb"; /* nome do seu banco de dados*/
+    $dbname = "hnrtco66_fvghml"; /* nome do seu banco de dados*/
 
     $port = "3306"; /* nome do seu banco de dados*/
     // Criando a conexão com o banco de dados
     $strcon = new mysqli($servername, $username, $password, $dbname, $port);
-
-    $connect = new PDO("mysql:host=localhost:3306;dbname=hnrtco66_fvgweb", $username, $password);
+    $connect = new PDO("mysql:host=localhost;dbname=hnrtco66_fvghml", $username, $password);
 
     // Checando a conexão com o banco de dados
 
-    if ($strcon->connect_error) {
-
-        die('Connect Error (' . $strcon->connect_errno . ') ' . $strcon->connect_error);
+	@@ -24,4 +23,4 @@
 
     } 
-    
+
 ?>
